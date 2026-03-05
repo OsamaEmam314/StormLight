0f84f2b2ebeb0aed3ad043ef5aae56ecce2b0038
Osama Khaled <osamakhaled31415@gmail.com>
2026-03-05 18:03:34 +0200

    Splash screen + Icon

--- app/src/main/java/com/example/stormlight/ui/MainActivity.kt
+++ app/src/main/java/com/example/stormlight/ui/MainActivity.kt
@@ -11,11 +11,15 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.stormlight.ui.theme.StormLightTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
+
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
         setContent {
             StormLightTheme {
